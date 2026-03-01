0559c5fee5793918f802f9a5a0e7e1d2e1a2b4e0
ysffend <fendiysf281@gmail.com>
2026-03-02 00:52:59 +0700

    Add files via upload

--- galeri.js
+++ galeri.js
@@ -1,24 +1,7 @@
-const cards = document.querySelectorAll(".card img");
-const preview = document.getElementById("preview");
-const previewImg = document.getElementById("previewImg");
-const closePreview = document.getElementById("closePreview");
+const cards = document.querySelectorAll(".card");
 
-// buka preview
-cards.forEach(img => {
-  img.addEventListener("click", () => {
-    preview.classList.add("active");
-    previewImg.src = img.src;
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    alert("Preview / Detail Photo (bisa dikembangkan)");
   });
-});
-
-// tutup preview
-closePreview.addEventListener("click", () => {
-  preview.classList.remove("active");
-});
-
-// klik background untuk tutup
-preview.addEventListener("click", (e) => {
-  if (e.target === preview) {
-    preview.classList.remove("active");
-  }
 });
